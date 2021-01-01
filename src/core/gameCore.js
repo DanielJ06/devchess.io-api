12a97657522ce88ef997f21ab21bafaf38cd5bab
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-01 18:54:38 -0300

    PlayerJoinGame event

--- src/core/gameCore.js
+++ src/core/gameCore.js
@@ -10,6 +10,7 @@ const initialize = (socketio, socket) => {
 
   gameSocket.on("disconnect", onDisconnect)
   gameSocket.on("createNewGame", createNewGame)
+  gameSocket.on("playerJoinGame")
 }
 
 const onDisconnect = () => {
@@ -20,6 +21,30 @@ const onDisconnect = () => {
 const createNewGame = (gameId) => {
   this.emit('createNewGame', { gameId: gameId, mySocketId: this.id })
   this.join(gameId)
+}
+
+const playerJoinGame = (id) => {
+  let socket = this;
+  let room = io.socket.adapter.rooms[id.gameId];
+
+  if(room === undefined) {
+    this.emit('status', 'This game session does not exists!');
+    return
+  }
+
+  if(room.length < 2) {
+    id.mySocketId = socket.id;
+
+    socket.join(id.gameId);
+
+    if (room.length === 2) {
+      io.socket.in(id.gameId).emit('start', id.username);
+    }
+
+    io.sockets.in(id.gameId).emit('playerJoinedRoom', id);
+  } else {
+    this.emit('status', 'There are already 2 players playing in this room')
+  }
 }
 
 exports.initialize = initialize;
