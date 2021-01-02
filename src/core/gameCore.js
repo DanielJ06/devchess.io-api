22da5cc378b77e4ffc8c86121a26e9ad56efae7b
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-02 09:31:14 -0300

    Code refactor

--- src/core/gameCore.js
+++ src/core/gameCore.js
@@ -1,8 +1,9 @@
 let io;
 let gameSocket;
 let gamesInSession = [];
+let users = []
 
-const initialize = (socketio, socket) => {
+function initialize(socketio, socket) {
   io = socketio;
   gameSocket = socket;
 
@@ -10,22 +11,28 @@ const initialize = (socketio, socket) => {
 
   gameSocket.on("disconnect", onDisconnect)
   gameSocket.on("createNewGame", createNewGame)
-  gameSocket.on("playerJoinGame")
+  gameSocket.on("playerJoinGame", playerJoinGame);
 }
 
-const onDisconnect = () => {
+function onDisconnect() {
   let i = gamesInSession.indexOf(gameSocket);
   gamesInSession.splice(i, 1);
 }
 
-const createNewGame = (gameId) => {
-  this.emit('createNewGame', { gameId: gameId, mySocketId: this.id })
-  this.join(gameId)
+function createNewGame(data) {
+  const user = {
+    username: data.username,
+    color: 'w'
+  }
+  users.push(user);
+  console.log('Game host:', users);
+  this.join(data.gameId)
 }
 
-const playerJoinGame = (id) => {
+function playerJoinGame(id) {
+  console.log(id.gameId);
   let socket = this;
-  let room = io.socket.adapter.rooms[id.gameId];
+  let room = io.sockets.adapter.rooms[id.gameId];
 
   if(room === undefined) {
     this.emit('status', 'This game session does not exists!');
@@ -36,9 +43,15 @@ const playerJoinGame = (id) => {
     id.mySocketId = socket.id;
 
     socket.join(id.gameId);
+    const user = {
+      username: id.username,
+      color: 'b'
+    }
+    users.push(user);
+    console.log('Join a game: ', users)
 
     if (room.length === 2) {
-      io.socket.in(id.gameId).emit('start', id.username);
+      io.sockets.in(id.gameId).emit('start', id.username);
     }
 
     io.sockets.in(id.gameId).emit('playerJoinedRoom', id);
