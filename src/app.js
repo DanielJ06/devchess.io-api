5d9e1cb14f0707ec6a47aa8c94ac31f359b51fec
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-02 09:30:31 -0300

    Enabling cors

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ const app = express();
 const gameCore = require('./core/gameCore');
 
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', client => {
   gameCore.initialize(io, client);
 });
 
-server.listen(process.env.PORT || 3333);
+server.listen(3333, () => console.log("Running..."));
